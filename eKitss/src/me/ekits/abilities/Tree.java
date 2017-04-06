05fd9d70f4a290ae25e714ec47440206425c2d09
Jakibah <iakopetrou@gmail.com>
2017-04-06 16:26:33 +0200

    Added Debug messages to fix Tree.

--- eKitss/src/me/ekits/abilities/Tree.java
+++ eKitss/src/me/ekits/abilities/Tree.java
@@ -54,6 +54,11 @@ public class Tree implements Listener{
 	            	Bukkit.getServer().getWorld(rightclick.getWorld().getName()).getBlockAt(treeloc).setType(Material.AIR);
 	    	    	Bukkit.getServer().getWorld(rightclick.getWorld().getName()).getBlockAt(treeloc2).setType(Material.AIR);
 	    	    	Bukkit.getServer().getWorld(rightclick.getWorld().getName()).getBlockAt(treeloc3).setType(Material.AIR);
+	    	    	p.sendMessage(rightclick.getWorld().getName());
+	    	    	p.sendMessage(	Bukkit.getServer().getWorld(rightclick.getWorld().getName()).getName());
+	    	    	p.sendMessage(treeloc.toString());
+	    	    	p.sendMessage(treeloc2.toString());
+	    	    	p.sendMessage(treeloc3.toString());
 	    	    	plugin.rooted.remove(rightclick.getName());
 	            }
 	    }, 300);
